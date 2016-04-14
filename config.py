e8607b28bcb44c464c6814acdca609623c4eb886
Alex Sands <acsands13@verizon.net>
2016-04-13 20:03:45 -0400

    Removed config from ignore

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+_basedir = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = False
+
+SECRET_KEY = 'SecretKeyForSessionSigning'
+
+password = 'nets213ccb'
+uri =  'mysql://admin:%s@nets213ccb.cxh68dehnct7.us-east-1.rds.amazonaws.com/nets213ccb' %(password)
+SQLALCHEMY_DATABASE_URI = uri
+DATABASE_CONNECT_OPTIONS = {}
+
+THREADS_PER_PAGE = 8
+
+CSRF_ENABLED=True
+CSRF_SESSION_KEY="somethingimpossibletoguess"
