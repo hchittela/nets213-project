de764ae82ab4b77d914e15b16562cf6402e3715b
ggupta13 <gagan.aj.gupta@gmail.com>
2016-04-27 18:51:13 -0400

    Aggregation output

--- src/qc1input_to_qc1output.py
+++ src/qc1input_to_qc1output.py
@@ -2,21 +2,22 @@ import csv
 
 bad_response_workers = []
 
-with open('../data/qc1_input.csv') as csvfile:
-	reader = csv.DictReader(csvfile)
-	for row in reader:
-		if row['_golden'] == 'TRUE' and row['which_design_is_better'] != row['which_design_is_better_gold']:
-			bad_response_workers.append(row['_worker_id'])
+# not sure why, but none of the results have golden = TRUE, so this is pointless, hence I commented it
+# with open('../data/f901004.csv') as csvfile:
+# 	reader = csv.DictReader(csvfile)
+# 	for row in reader:
+# 		if row['_golden'].upper() == 'TRUE' and row['select_the_most_visually_appealing_design'] != row['select_the_most_visually_appealing_design']:
+# 			bad_response_workers.append(row['_worker_id'])
 
 
 with open('../data/qc1_output.csv', 'wb') as writefile:
 	qc1_output = csv.writer(writefile)
 	qc1_output.writerow(['img_id','vote'])
-	with open('../data/qc1_input.csv') as csvfile:
+	with open('../data/f901004.csv') as csvfile:
 		reader = csv.DictReader(csvfile)
 		for row in reader:
-			if row['_golden'] == 'FALSE' and row['_worker_id'] not in bad_response_workers:
+			if row['_golden'].upper() == 'FALSE' and row['_worker_id'] not in bad_response_workers:
 				data = []
-				data.append(row['img_id'])
-				data.append(row['which_design_is_better'])
+				data.append(row['_unit_id'])
+				data.append(row['select_the_most_visually_appealing_design'])
 				qc1_output.writerow(data)
