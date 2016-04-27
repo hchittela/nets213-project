544a6c1580502569471e8c090b3750adf008455e
ggupta13 <gagan.aj.gupta@gmail.com>
2016-04-27 17:27:42 -0400

    changes to qc2input

--- src/qc1input_to_qc2input.py
+++ src/qc1input_to_qc2input.py
@@ -2,39 +2,40 @@ import csv
 
 url_comments = {}
 
-with open('../data/qc1_input.csv') as csvfile:
+with open('../data/f901004.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
-		if row['_golden'] == 'FALSE':
+		print row
+		if row['_golden'].upper() == 'FALSE':
 			if row['url_img1'] in url_comments:
-				url_comments[row['url_img1']].append(row['comment_img1'])
+				url_comments[row['url_img1']].append(row['enter_the_reasoning_for_your_decision'])
 			else:
 				url_comments[row['url_img1']] = []
-				url_comments[row['url_img1']].append(row['img_id'])
-				url_comments[row['url_img1']].append(row['comment_img1'])
-			if row['url_img2'] in url_comments:
-				url_comments[row['url_img2']].append(row['comment_img2'])
-			else:
-				url_comments[row['url_img2']] = []
-				url_comments[row['url_img2']].append(row['img_id'])
-				url_comments[row['url_img2']].append(row['comment_img2'])
+				url_comments[row['url_img1']].append(row['_unit_id'])
+				url_comments[row['url_img1']].append(row['enter_the_reasoning_for_your_decision'])
+			# if row['url_img2'] in url_comments:
+			# 	url_comments[row['url_img2']].append(row['comment_img2'])
+			# else:
+			# 	url_comments[row['url_img2']] = []
+			# 	url_comments[row['url_img2']].append(row['_unit_id'])
+			# 	url_comments[row['url_img2']].append(row['comment_img2'])
 
 print url_comments
 
 
 with open('../data/qc2_input.csv', 'wb') as writefile:
 	qc2_input = csv.writer(writefile)
-	qc2_input.writerow(['id','img_id','img_url','img_comment1','img_comment2','img_comment3','img_comment4','img_comment5'])
+	qc2_input.writerow(['img_id','img_url','img_comment1','img_comment2','img_comment3','img_comment4','img_comment5'])
 	for key, value in url_comments.iteritems():
-		data = []
-		data.append('1')
-		data.append(value[0])
-		data.append(key)
-		data.append(value[1])
-		data.append(value[2])
-		data.append(value[3])
-		data.append(value[4])
-		data.append(value[5])
-		qc2_input.writerow(data)
+		for i in range(0,len(value)/5):
+			data = []
+			data.append(value[0])
+			data.append(key)
+			data.append(value[i*5 + 1])
+			data.append(value[i*5 + 2])
+			data.append(value[i*5 + 3])
+			data.append(value[i*5 + 4])
+			data.append(value[i*5 + 5])
+			qc2_input.writerow(data)
 
 
